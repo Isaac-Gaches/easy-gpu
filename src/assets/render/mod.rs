6e306b1866290651e49c90676def32d382c4600c
Isaac Gaches <isaacgaches27@gmail.com>
2026-05-25 16:51:36 +0100

    improved instanced rendering

--- src/assets/render/mod.rs
+++ src/assets/render/mod.rs
@@ -2,4 +2,5 @@
 pub(crate) mod material;
 pub(crate) mod mesh;
 pub(crate) mod pipeline;
+pub(crate) mod task;
 
--- src/frame.rs
+++ src/frame.rs
@@ -1,4 +1,5 @@
 use std::ops::Range;
+use bytemuck::bytes_of;
 use crate::assets::buffer::Buffer;
 use crate::assets::compute::bind_group::ComputeBindGroup;
 use crate::assets::compute::pipeline::ComputePipeline;
@@ -6,31 +7,14 @@ use crate::assets::compute::task::ComputeTask;
 use crate::assets_manager::handle::Handle;
 use crate::assets::render::material::Material;
 use crate::assets::render::mesh::Mesh;
-use crate::assets::Texture;
-
-pub(crate) struct RenderItem {
-    pub mesh: Handle<Mesh>,
-    pub material: Handle<Material>,
-    pub instances: Option<Handle<Buffer>>,
-    pub range: Option<Range<u32>>,
-
-}
-
-impl RenderItem {
-    fn new(mesh: Handle<Mesh>, material: Handle<Material>,instances: Option<Handle<Buffer>>,range: Option<Range<u32>>) -> Self {
-        Self{
-            mesh,
-            material,
-            instances,
-            range,
-        }
-    }
-}
+use crate::assets::{GpuInstance, Texture};
+use crate::assets::render::task::{DrawCommand, InstancedCommand, RenderTask, StreamedCommand};
 
 pub struct Frame {
-    pub(crate) render_tasks: Vec<RenderItem>,
+    pub(crate) render_tasks: Vec<RenderTask>,
     pub(crate) compute_tasks: Vec<ComputeTask>,
     pub(crate) textures_to_clear: Vec<Handle<Texture>>,
+    pub(crate) instance_bytes: Vec<u8>,
 }
 
 impl Frame {
@@ -38,7 +22,8 @@ impl Frame {
         Self {
             render_tasks: Vec::new(),
             compute_tasks: Vec::new(),
-            textures_to_clear: vec![],
+            textures_to_clear: Vec::new(),
+            instance_bytes: Vec::new(),
         }
     }
 
@@ -47,15 +32,39 @@ impl Frame {
         self.compute_tasks.clear();
     }
 
-    pub fn draw_mesh(&mut self,material: Handle<Material>,mesh: Handle<Mesh>) {
-        let item = RenderItem::new(mesh, material,None,None);
+    pub fn draw(&mut self,material: Handle<Material>,mesh: Handle<Mesh>) {
+        let item = RenderTask::Draw(DrawCommand{
+            mesh,
+            material,
+        });
         self.render_tasks.push(item);
     }
 
-    pub fn draw_instances(&mut self,instances: Handle<Buffer>,material: Handle<Material>,mesh: Handle<Mesh>,range: Range<u32>) {
-        let item = RenderItem::new(mesh, material, Some(instances), Some(range));
+    pub fn draw_manual_batch(&mut self,instances: Vec<Handle<Buffer>>,material: Handle<Material>,mesh: Handle<Mesh>,range: Range<u32>) {
+        let item = RenderTask::DrawStreamed(StreamedCommand{
+            mesh,
+            material,
+            instances,
+            range,
+        });
         self.render_tasks.push(item);
     }
+
+    pub fn draw_batch<T: GpuInstance>(&mut self,instance: &[T],material: Handle<Material>,mesh: Handle<Mesh>){
+        let instance_count = instance.len() as u32;
+        let start = self.instance_bytes.len();
+        self.instance_bytes.extend_from_slice(bytes_of(&instance));
+        let end = self.instance_bytes.len();
+        let instance_range = start as u32..end as u32;
+
+        let item = RenderTask::DrawInstanced(InstancedCommand{
+            mesh,
+            material,
+            instance_range,
+            instance_count,
+        });
+
+        self.render_tasks.push(item);    }
 
     pub fn compute(&mut self,bind_group: Handle<ComputeBindGroup>, pipeline: Handle<ComputePipeline>, dispatch: (u32,u32,u32)){
         let task = ComputeTask::new(pipeline,bind_group,dispatch);

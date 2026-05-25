4dbc70ee45e6f440b85312acae733ea5138dbe63
Isaac Gaches <isaacgaches27@gmail.com>
2026-05-25 18:39:19 +0100

    fixed handle impls

--- src/assets_manager/handle.rs
+++ src/assets_manager/handle.rs
@@ -1,8 +1,26 @@
-#[derive(Debug, Hash,Eq)]
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug)]
 pub struct Handle<T> {
     pub index: u32,
     pub generation: u32,
     pub _marker: std::marker::PhantomData<T>,
+}
+
+impl<T> PartialEq for Handle<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index &&
+            self.generation == other.generation
+    }
+}
+
+impl<T> Eq for Handle<T> {}
+
+impl<T> Hash for Handle<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
+        self.generation.hash(state);
+    }
 }
 
 impl<T> Copy for Handle<T> {}
@@ -13,10 +31,5 @@ impl<T> Clone for Handle<T> {
     }
 }
 
-impl<T> PartialEq for Handle<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.index == other.index &&
-        self.generation == other.generation
-    }
-}
+
 
